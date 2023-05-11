858f1c0711427bcb368f014712539ae83063b994
Shanzzz <shahnazafreen9849@gmail.com>
2023-05-10 22:48:13 -0400

    added  content to tables

---
+++ db/seeds.sql
@@ -0,0 +1,16 @@
+INSERT INTO movies(movie_name)
+VALUES("Lion King"),
+("The GodFather"),
+("West Side Story"),
+("Parasite"),
+("The Wizard of Oz");
+
+
+INSERT INTO reviews(movie_id, review)
+VALUES(1, "Zazu is underrated. Give that horn bill a sequel!"),
+(2, "I am gonna make him an offer you can't refuse, watch this movie"),
+(3, "Scar is the lion everyone loves to hate"),
+(4, "Ten year of ballet and three years of tap to join a gang in this neighborhood"),
+(5, "The tin man gave a metallic, hollow performance"),
+(1, "Hakuna matata"),
+(5, "Those flying monkeys are nightmare fuel!");
